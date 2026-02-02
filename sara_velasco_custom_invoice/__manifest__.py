4655c7be1cfd231f4c5518658ffa1be8346c18c4
EndikaPM <endika.perez.mas@gmail.com>
2026-02-02 11:50:43 +0100

    Sync

---
+++ sara_velasco_custom_invoice/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Sara Velasco Custom Invoice",
+    "version": "19.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Customization of invoice external layout bubble",
+    "description": """
+        Este módulo hereda y personaliza la vista web.external_layout_bubble
+        para facturas personalizadas de Sara Velasco.
+    """,
+    "author": "Sara Velasco",
+    "website": "",
+    "license": "LGPL-3",
+    "depends": ["web", "account"],
+    "data": [
+        "views/external_layout_bubble.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
